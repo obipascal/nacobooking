cdd94a4b2e394be6ab123f81690e323e34f4da78
gaddafi <gaddafiadamu400@gmail.com>
2024-04-30 22:28:37 +0100

    fix seo

--- modules/Layout/app.blade.php
+++ modules/Layout/app.blade.php
@@ -47,6 +47,20 @@
       gtag('config', 'G-3F6J4G0G1Z');
     </script>
 
+
+    {{-- google tag gaskid --}}
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16458592239">
+    </script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'AW-16458592239');
+    </script>
+
     
     
     <script>
